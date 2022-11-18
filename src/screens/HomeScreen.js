88bb91b6f9e62a6cc0d3fa3b5ff7e6de3006c07f
okstas86 <stanislavocunev1@gmail.com>
2022-11-18 11:15:41 +0200

    v1.0

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import products from "../products.js";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={products._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default HomeScreen;
